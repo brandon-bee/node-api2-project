6c97d86ee321c0585eaa1f0d2d705bc56649e97a
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-08 13:20:11 -0800

    finsihed PUT

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -42,8 +42,11 @@ router.post('/', (req, res) => {
     });
   } else {
     Post.insert(body)
-      .then(newPost => {
-        res.status(201).json(newPost);
+      .then(post => {
+        res.status(201).json({
+          id: post.id,
+          ...body
+        });
       })
       .catch(() => {
         res.status(500).json({
@@ -52,5 +55,36 @@ router.post('/', (req, res) => {
       });
   }
 });
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  if (!body.title || !body.contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    });
+  } else {
+    Post.update(id, body)
+      .then(updatedPost => {
+        if (!updatedPost) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          });
+        } else {
+          res.status(200).json({
+            ...body,
+            id: Number(id)
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "The post information could not be modified"
+        });
+      });
+  }
+});
+
+
 
 module.exports = router;
