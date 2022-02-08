92b6e3bc823eb1a748c41720027050387381d567
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-08 11:20:34 -0800

    Completed GET endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,3 +2,35 @@
 const Post = require('./posts-model');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved"
+      });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Post.findById(id)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist"
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "The post information could not be retrieved"
+      });
+    });
+});
+
+module.exports = router;
