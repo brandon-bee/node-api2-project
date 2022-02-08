4ab29f314497fd3d5a2b1d5ba0695c09e4f0d554
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-08 14:34:37 -0800

    Achieved MVP, passing all tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -85,6 +85,51 @@ router.put('/:id', (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      });
+    } else {
+      await Post.remove(id);
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed"
+    });
+  }
+});
 
+router.get('/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      });
+    } else {
+      Post.findPostComments(id)
+        .then(comments => {
+          if (comments.length > 0) {
+            res.status(200).json(comments);
+          }
+        })
+        .catch(() => {
+          res.status(500).json({
+            message: "The comments information could not be retrieved"
+          });
+        })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The comments information could not be retrieved"
+    });
+  }
+});
 
 module.exports = router;
