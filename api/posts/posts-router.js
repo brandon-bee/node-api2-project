9990935c4ee443b853c2a9f73650f33dcd95f0e3
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-08 11:01:46 -0800

    Created and connected initial files

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,4 @@
 // implement your posts router here
+const Post = require('./posts-model');
+const router = require('express').Router();
+
--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
+const express = require('express');
+const server = express();
+server.use(express.json());
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+server.use('*', (req, res) => {
+  res.status(404).send(`
+    <p>Oops, we can not find that resource</p>
+  `);
+});
+
+module.exports = server;
